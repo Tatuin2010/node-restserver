e701205e905e776da28812c1638a22ef82f362fd
Tatuin <tatuin2010@hotmail.com>
2021-01-30 14:33:09 +0100

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-require('./config/config')
-const express = require('express')
-const app = express()
+require('./config/config');
+
+
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
 const bodyParser = require('body-parser')
     // parse application/x-www-form-urlencoded
@@ -8,34 +11,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json(body)
+
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        throw err;
     }
-})
+    console.log('Base de Datos online');
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
-
-
+});
 
 
 app.listen(process.env.PORT, () => {
